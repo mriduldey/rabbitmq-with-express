0e0116e9c20bdd28c2406a320080b93d848951b6
Mridul Dey <51965740+mriduldey@users.noreply.github.com>
2022-07-22 13:25:50 +0530

    post api for sendng message implemented post api for sendng message implemented

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const sendMessage = require("./services/MQService");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  // res.json({ appName: "Workinging with RabbitMQ" });
-  res.send(Buffer.from("<p>some html</p>"));
+  res.set("Content-Type", "text/html");
+  res.send(Buffer.from("<p>RabbitMQ Practice with Node</p>"));
+});
+
+app.post("/", (req, res) => {
+  try {
+    const { message, queue } = req.body;
+    sendMessage(message, queue);
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
 app.listen(3000, () => console.log("server running on 3000"));
