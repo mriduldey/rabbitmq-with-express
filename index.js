bd56649f21674a19f33c69dc05b19d49c0fb76ec
Mridul Dey <mridul.dey@ellucian.com>
2022-07-22 15:39:21 +0530

    simple rabbitmq post data implementation added

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const express = require("express");
-const sendMessage = require("./services/MQService");
+const express = require('express');
+const { sendMessage } = require('./services/MQService');
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.set("Content-Type", "text/html");
-  res.send(Buffer.from("<p>RabbitMQ Practice with Node</p>"));
+app.get('/', (req, res) => {
+  res.set('Content-Type', 'text/html');
+  res.send('<p>RabbitMQ Practice with Node</p>');
 });
 
-app.post("/", (req, res) => {
+app.post('/', (req, res) => {
   try {
     const { message, queue } = req.body;
-    sendMessage(message, queue);
+    sendMessage(queue, message);
+    res.status(200).send('Message Sending Successful');
   } catch (err) {
     console.log(err.message);
   }
 });
 
-app.listen(3000, () => console.log("server running on 3000"));
+app.listen(3000, () => console.log('server running on 3000'));
--- services/MQService.js
+++ services/MQService.js
@@ -1,13 +1,21 @@
-import amqp from "amqplib";
+const amqp = require('amqplib');
 
 const AMQP_CONN_URL = `amqps://${process.env.amqpUser}:${process.env.amqpPass}@puffin.rmq2.cloudamqp.com/${process.env.amqpUser}`;
 
 const amqpConn = amqp.connect(AMQP_CONN_URL);
 
-export function sendMessage(q, msg) {
-  amqpConn.then(async (conn) => {
-    const channel = await conn.createChannel();
-    await channel.assertQueue(q);
-    channel.sendToQueue(q, Buffer.from(msg));
-  });
+function sendMessage(q, msg) {
+  amqpConn
+    .then(async (conn) => {
+      const channel = await conn.createChannel();
+      await channel.assertQueue(q);
+      channel.sendToQueue(q, Buffer.from(JSON.stringify(msg)));
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
+
+module.exports = {
+  sendMessage,
+};
